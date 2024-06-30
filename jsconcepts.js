b4e3751d32ed9205c71172bdbd9855f0e839e494
SHK KAFEEL <137709969+shkkafeel@users.noreply.github.com>
2024-06-30 11:12:18 +0530

    Add files via upload

---
+++ jsconcepts.js
@@ -0,0 +1,70 @@
+// start old concept
+
+// let pencilPrice = 10;
+// let eraserPrise = 5;
+// let output = " The total price is : " + (pencilPrice + eraserPrise) + " Rupees"
+
+// end old concept
+
+
+// start new concept
+
+// Below is the example of a Template Literal
+// let output = (`The total price is : ${pencilPrice + eraserPrise} Rupees.`);
+// console.log(output);
+
+// end new concept
+
+
+// Start Arithmetic Operators
+
+// let a = 17;
+// let b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// console.log(a++);
+
+// End Arithmetic Operators
+
+
+// start old concept:
+
+// let year = 1999;
+// year += 1;
+
+// console.log(year);
+// year += 1;
+// console.log(year);
+
+// end old concept
+
+
+// start new concept
+
+// let age = 10;
+// let newAge = ++age;
+// console.log(age);
+// console.log(newAge);
+
+// newAge = age++;
+// console.log(newAge);
+// console.log(age);
+
+// end new concept
+
+// start new concept 2;
+
+// let y = 10;
+// let k = 5;
+// console.log(y++);
+// console.log(y++);
+// console.log(++y);
+// console.log(++y);
+
+// end new concept 2;
