fc90627f4ac12daaf417d50961b41b7ddf07d3b8
SHK KAFEEL <137709969+shkkafeel@users.noreply.github.com>
2024-07-07 14:57:19 +0530

    Add files via upload

---
+++ ifelseif.js
@@ -0,0 +1,34 @@
+
+// Code will Run on Terminal
+
+// Popcorn Price
+
+// let size = "S";
+// if (size === "XL") {
+//     console.log("price is Rs. 250")
+// } else if (size === "L") {
+//     console.log("price is Rs. 200")
+// } else if (size === "M") {
+//     console.log("price is Rs. 100")
+// } else {
+//     console.log("price is Rs. 50")
+// }
+
+
+
+// Nested Levels Inside If Else Statement
+
+// Student Grades & Marks
+
+let marks = 90;
+
+if (marks >= 33) {
+    console.log("Pass");
+    if(marks >=80) {
+        console.log("Grade : O");
+    } else {
+        console.log("Grade : A");
+    }
+} else {
+    console.log("Better luck Next Time");
+}
